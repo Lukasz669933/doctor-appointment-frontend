4860d144795441b2bff1832d564140258c6481ec
Lukasz669933 <125608883+Lukasz669933@users.noreply.github.com>
2024-04-18 06:29:31 +0000

    minor

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: false,
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
