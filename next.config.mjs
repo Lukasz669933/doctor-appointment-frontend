849652cda9433cec7c06927116294b0f8e71e1bd
Lukasz669933 <125608883+Lukasz669933@users.noreply.github.com>
2024-04-18 06:22:52 +0000

    fixed the bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: false,
   images: {
     domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
